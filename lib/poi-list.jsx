57126794aee399533e628709b71980bdc5051457
zach pratt <zach@attackofzach.com>
2015-02-22 02:04:55 -0600

    Clean up the public API

--- lib/poi-list.jsx
+++ lib/poi-list.jsx
@@ -5,6 +5,39 @@
 
         React = require('react');
 
+    function handleFilter(event) {
+        var filterInput = event.currentTarget.value,
+            numberOfVisiblySelectedItems = 0;
+
+        this.props.collection.each(function (model) {
+            var isVisible = (model.get('name').indexOf(filterInput) > -1);
+
+            if (model.get('active') && isVisible) {
+                numberOfVisiblySelectedItems += 1;
+            }
+        });
+
+        this.setState({
+            filter: filterInput,
+            itemsChecked: numberOfVisiblySelectedItems
+        });
+    }
+
+    function renderChildren() {
+        var collection = this.props.collection,
+            filterInput = this.state.filter;
+
+        return collection
+            .filter(function (model) {
+                return (model.get('name').indexOf(filterInput) > -1);
+            })
+            .map(function (model) {
+                return (
+                    <PoiItem key={model.id} model={model} />
+                );
+            }, this);
+    }
+
     module.exports = React.createClass({
         getInitialState: function () {
             return {
@@ -30,46 +63,13 @@
         render: function () {
             return (
                 <form>
-                    <input type="text" onChange={this.filter} />
+                    <input type="text" onChange={handleFilter.bind(this)} />
                     <ul>
-                        {this.renderChildren()}
+                        {renderChildren.call(this)}
                     </ul>
                     <h2>{'Checked Items: ' + this.state.itemsChecked}</h2>
                 </form>
             );
-        },
-
-        renderChildren: function () {
-            var collection = this.props.collection,
-                filterInput = this.state.filter;
-
-            return collection
-                .filter(function (model) {
-                    return (model.get('name').indexOf(filterInput) > -1);
-                })
-                .map(function (model) {
-                    return (
-                        <PoiItem key={model.id} model={model} />
-                    );
-                }, this);
-        },
-
-        filter: function (event) {
-            var filterInput = event.currentTarget.value,
-                numberOfVisiblySelectedItems = 0;
-
-            this.props.collection.each(function (model) {
-                var isVisible = (model.get('name').indexOf(filterInput) > -1);
-
-                if (model.get('active') && isVisible) {
-                    numberOfVisiblySelectedItems += 1;
-                }
-            });
-
-            this.setState({
-                filter: filterInput,
-                itemsChecked: numberOfVisiblySelectedItems
-            });
         }
     });
-}());
+})();
